1c2c694ffd1b22505c159c735f6c057d1b57aa1d
shivam patel <shivampatel@shivams-MacBook-Pro.local>
2024-07-29 22:39:00 +0530

    added file 2

---
+++ f2.py
@@ -0,0 +1,12 @@
+# Open the original file in read mode
+with open('file1.txt', 'r') as file:
+    content = file.read()
+
+# Make changes to the content
+new_content = content.replace('old_text', 'new_text')
+
+# Save the modified content to a new file
+with open('file2.txt', 'w') as new_file:
+    new_file.write(new_content)
+
+print("Changes saved to file2.txt")
